477ac7d50b0ffa3e2cebc2921344a2350dd53c8a
lilvina <davinaleo2007@yahoo.com>
2019-01-29 13:35:18 -0800

    nav buttons change color to blue and it works

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+let navButton = document.querySelectorAll('.nav .nav-link')
+navButton.forEach(element => {
+  element.addEventListener('mouseover', event => {
+    event.target.style.color = "blue"
+    console.log(`This is my target: ${event.target}`)
+    setTimeout(function () {
+      event.target.style.color = ""
+    }, 500)
+  }, false)
+
+})
